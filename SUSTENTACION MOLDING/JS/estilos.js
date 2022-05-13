e0bafe61ca27ac5b2692b699bd2e963c11fb463e
bdherrera <braherrera2003@hotmail.com>
2022-05-13 10:33:04 -0500

    código página cliente
    Co-Authored-By: Sindi Rodriguez <99542046+SIMIRO1109@users.noreply.github.com>

---
+++ SUSTENTACION MOLDING/JS/estilos.js
@@ -0,0 +1,12 @@
+const app = document.getElementById('typewriter');
+
+const typewriter =new typewriter(app, {
+loop: true, 
+delay: 75
+})
+
+typewriter
+.typeString("La capital del sol")
+.pauseFor (200)
+.start();
+
